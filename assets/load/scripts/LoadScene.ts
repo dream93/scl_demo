0f50505b537dd39e811003c28a566c53b2150e2a
dream <dream_chou93@163.com>
2021-04-19 17:07:29 +0800

    同步scl库

--- assets/load/scripts/LoadScene.ts
+++ assets/load/scripts/LoadScene.ts
@@ -1,13 +1,13 @@
 
 import { _decorator, Component, Node, director } from 'cc';
-import { PopupManager } from '../../libs/popup/manager/PopupManager';
+import { App } from '../../libs/App';
 const { ccclass, property } = _decorator;
 
 @ccclass('LoadScene')
 export class LoadScene extends Component {
 
     start() {
-        PopupManager.instance.init();
+        App.instance.init();
         director.preloadScene('Test', () => {
             director.loadScene('Test');
         });
